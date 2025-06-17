76ace7985184feaa58665d7168f6b1ef9c546b4f
Negasi Gidena <negasig21@gmail.com>
2025-06-17 13:30:46 +0300

    add file

--- src/module.ts
+++ src/module.ts
@@ -1,9 +1,20 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './controller';
 import { AppService } from './service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from './model/user';
 
 @Module({
-  imports: [],
+  imports: [TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: 'root',
+      password: '',
+      database: 'testng',
+      entities: [User],
+      synchronize: true,
+    }),],
   controllers: [AppController],
   providers: [AppService],
 })
